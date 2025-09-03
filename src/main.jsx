19ea16328c1c5cf2528985240922c1db252eadf5
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2025-09-03 16:31:19 -0400

    WordChainGame

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import WordChainGame from './WordChainGame.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WordChainGame />
   </StrictMode>,
 )
