dc8459a85d4a2763b75c245c71a3a4d2eb69b2b4
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2025-09-09 19:14:56 -0400

    good lord

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import WordChainGame from "./WordChainGame.jsx"
+import WordChainGame from "./WordChainGame.jsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
